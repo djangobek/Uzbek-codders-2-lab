739e3fbcdbec6d1e102c58831e500f6b860b3274
Nurbek <98734560+nurbekchik@users.noreply.github.com>
2022-12-27 16:38:13 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+# 1 test
+text = "again"
+def reverse(text):
+    #complete this function so that it takes string x as an input 
+    #and returns its reverse
+    a = text
+    return a[::-1]
+print("the reversed text is: "+reverse(text))
+
+
+#  2 - test
+no_list = [22,68,90,78,90,88]
+def average(x):
+    #complete the function's body to return the average
+    a = (no_list[0]+no_list[1]+no_list[2]+no_list[3]+no_list[4]+no_list[5])/6
+    return a
+print(average(no_list))
+
+
+# 3 - test
+no_list = [1,2,3,4]
+
+def maximum(no_list):
+    #complete the function to return the highest number in the list
+    a = max(no_list)
+    return a
+print(maximum(no_list))
+
+
+#  4 - test
+no_list = [22,22,2,1,11,11,2,2,3,3,3,4,5,5,5,55,55,66]
+def unique_list(list1):
+    #complete the function's body to return the unique list of numbers
+    a = set(list1)
+    return list(dict.fromkeys(list1))
+print(unique_list(no_list))
